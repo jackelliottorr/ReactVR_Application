372f84b37ce13e514721324695dd7452080670bb
Vandalaz <jackelliottorr@protonmail.com>
2020-02-22 22:20:35 +0000

    Login Scene
    - Great progress in the Login Scene using VR UIKit.
    - Checking in now before I try to upgrade to new version of Unity which looks a lot nicer.

---
+++ ReactVR_Application/Assets/UIKits/Scripts/UI/Keyboard/Keys/Key.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+#if UIKIT_TMP
+using TMPro;
+#endif
+namespace VRUiKits.Utils
+{
+    public class Key : MonoBehaviour, IPointerDownHandler
+    {
+#if UIKIT_TMP
+    protected TextMeshProUGUI key;
+#else
+    protected Text key;
+#endif
+        public delegate void OnKeyClickedHandler(string key);
+
+        // The event which other objects can subscribe to
+        // Uses the function defined above as its type
+        public event OnKeyClickedHandler OnKeyClicked;
+
+        public virtual void Awake()
+        {
+#if UIKIT_TMP
+            if (transform.Find("Text").GetComponent<TextMeshProUGUI>())
+            {
+                key = transform.Find("Text").GetComponent<TextMeshProUGUI>();
+            }
+            else
+            {
+                Debug.LogError("Could not find TextMeshProUGUI Component on the key in Keyboard."
+                + " Please make sure to convert Text to TextMeshPro.");
+            }
+#else
+            key = transform.Find("Text").GetComponent<Text>();
+#endif
+            // GetComponent<Button>().onClick.AddListener(() =>
+            // {
+            //     OnKeyClicked(key.text);
+            // });
+        }
+
+        public void OnPointerDown(PointerEventData pointerEventData)
+        {
+            //Output the name of the GameObject that is being clicked
+            OnKeyClicked(key.text);
+        }
+
+        public virtual void CapsLock(bool isUppercase) { }
+        public virtual void ShiftKey() { }
+    }
+}
---
+++ ReactVR_Application/Assets/_PROJECT/Scripts/UIManagers/LoginManager.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Assets._PROJECT.Scripts.HelperClasses;
+using Newtonsoft.Json;
+using ReactVR_API.Common.Models;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoginManager : MonoBehaviour
+{
+    #region Private Fields
+    
+    [SerializeField]
+    private InputField _inputEmailAddress;
+
+    [SerializeField]
+    private InputField _inputPassword;
+
+    #endregion
+
+    #region Public Methods
+
+    private UserAccountCreateModel BuildLoginModel()
+    {
+        //var emailAddress = _inputEmailAddress.text;
+        //var password = _inputPassword.text;
+        var emailAddress = "jackelliottorr@gmail.com";
+        var password = "password1";
+        var passwordConfirm = "password1";
+
+        var userAccountCreateModel = new UserAccountCreateModel()
+        {
+            EmailAddress = emailAddress,
+            Password = password
+        };
+
+        return userAccountCreateModel;
+    } 
+
+    public bool HandleLoginAttempt(UserAccountCreateModel userAccount, string url)
+    {
+        try
+        {
+            var jsonString = JsonConvert.SerializeObject(userAccount);
+            var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+
+            using (var httpClient = new HttpClient())
+            {
+                //httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + accessToken);
+                var jsonResponse = httpClient.PostAsync(new Uri(url), content).Result;
+
+                if (jsonResponse.IsSuccessStatusCode)
+                {
+                    var responseContent = jsonResponse.Content.ReadAsStringAsync();
+                    string jwt = responseContent.Result;
+
+                    TokenManager tokenManager = new TokenManager();
+                    tokenManager.StoreToken(jwt);
+
+                    return true;
+                }
+                else
+                {
+                    var responseContent = jsonResponse.Content.ReadAsStringAsync();
+                    string errorMessage = responseContent.Result;
+                    return false;
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            var message = ex.Message;
+            throw ex;
+        }
+    }
+
+    public void Login()
+    {
+        var userAccountCreateModel = BuildLoginModel();
+        string url = "http://localhost:7071/api/UserAccount/Login";
+
+        if (HandleLoginAttempt(userAccountCreateModel, url))
+        {
+            SceneManager.LoadSceneAsync("Main Menu Scene", LoadSceneMode.Single);
+        }
+    }
+
+    public void CreateAccount()
+    {
+        var userAccountCreateModel = BuildLoginModel();
+        string url = "http://localhost:7071/api/UserAccount/CreateUserAccount";
+
+        if (HandleLoginAttempt(userAccountCreateModel, url))
+        {
+            SceneManager.LoadSceneAsync("Main Menu Scene", LoadSceneMode.Single);
+        }
+    }
+
+    #endregion
+}
